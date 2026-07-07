03bac5955a87f6cb8e95d35315fa45fc7783966c
jhzzin <joaohenriquepou4@gmail.com>
2026-07-06 21:39:30 -0300

    CalculadoraRetro Version-0.0.2 - Mudança no JS, melhor organização usando funções e lógica para impedir operadores duplicados

--- js/script.js
+++ js/script.js
@@ -1,30 +1,45 @@
 const display = document.getElementById('display');
 const botoes = document.querySelectorAll('button');
 
+const operadores = ['+', '-', '/', 'X']
+
+function limparDisplay() {
+    display.innerText = '0'
+}
+
+function apagarUltimoCaractere() {
+    if (display.innerText.length > 1) display.innerText = display.innerText.slice(0,-1);
+    else display.innerText = '0';
+}
+
+function calcularResultado() {
+    const expressao = display.innerText.replace(/X/g,'*');
+
+    try {
+        display.innerText = eval(expressao);
+    } catch {
+        display.innerText = 'ERROR';
+    }
+}
+
+function adicionarCaractere(caractere) {
+    const ultimoCaractere = display.innerText.slice(-1);
+
+    if (operadores.includes(caractere) && operadores.includes(ultimoCaractere)) {
+        display.innerText = display.innerText.slice(0,-1) + caractere;
+    return;
+    } 
+
+    if (display.innerText === '0') display.innerText = caractere;
+    else display.innerText += caractere;
+}
+
 botoes.forEach(botao => {
     botao.addEventListener('click', () => {
         const textoBotao = botao.innerText;
-
-        if (textoBotao === 'C') display.innerText = 0;
-
-        else if (textoBotao === 'DEL') {
-            if (display.innerText.length > 1) display.innerText = display.innerText.slice(0,-1);
-            else display.innerText = '0';
-        }
-
-        else if (textoBotao === '=') {
-            const expressao = display.innerText.replace(/x/g, '*');
-
-            try {
-                display.innertext = eval(expressao);
-            } catch {
-                display.innerText = 'ERROR';
-            }
-        }
-
-        else {
-            if (display.innerText === '0') display.innerText = textoBotao;
-            else display.innerText += textoBotao;
-        }
+        if (textoBotao === 'C') limparDisplay();
+        else if (textoBotao === 'DEL') apagarUltimoCaractere();
+        else if (textoBotao === '=') calcularResultado();
+        else adicionarCaractere(textoBotao);
     })
 })
